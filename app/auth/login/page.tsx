c54ee9dfffd57ae8f0abd50e781c298caf2218b5
Abdisa.dev <meetabdisa@gmail.com>
2025-12-28 20:14:47 +0300

    Finish Authentication

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signUpSchema } from "@/app/schemas/auth";
+import { loginSchema } from "@/app/schemas/auth";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -25,27 +25,25 @@ import { Controller, useForm } from "react-hook-form";
 import { toast } from "sonner";
 import z from "zod";
 
-export default function SignUpPage() {
+export default function LoginPage() {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const form = useForm({
-    resolver: zodResolver(signUpSchema),
+    resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
       password: "",
-      name: "",
     },
   });
 
-  function onSubmit(data: z.infer<typeof signUpSchema>) {
+  function onSubmit(data: z.infer<typeof loginSchema>) {
     startTransition(async () => {
-      await authClient.signUp.email({
+      await authClient.signIn.email({
         email: data.email,
         password: data.password,
-        name: data.name,
         fetchOptions: {
           onSuccess: () => {
-            toast.success("Account created successfully ");
+            toast.success("Logged in successfully ");
             router.push("/");
           },
           onError: (error) => {
@@ -59,30 +57,12 @@ export default function SignUpPage() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Sign Up</CardTitle>
-        <CardDescription>Create an account to get started</CardDescription>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Welcome Back!</CardDescription>
       </CardHeader>
       <CardContent>
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <FieldGroup className="gap-y-4">
-            <Controller
-              name="name"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field>
-                  <FieldLabel>Full Name</FieldLabel>
-                  <Input
-                    aria-invalid={fieldState.invalid}
-                    placeholder="John wick"
-                    type="text"
-                    {...field}
-                  />
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
             <Controller
               name="email"
               control={form.control}
@@ -122,11 +102,11 @@ export default function SignUpPage() {
             <Button disabled={isPending}>
               {isPending ? (
                 <>
-                  <Loader2 className="animate-spin size-4" />
+                  <Loader2 className="size-4 animate-spin" />
                   <span>Loading...</span>
                 </>
               ) : (
-                <span>Sign Up</span>
+                <span>Login</span>
               )}
             </Button>
           </FieldGroup>
--- components/web/navbar.tsx
+++ components/web/navbar.tsx
@@ -5,9 +5,14 @@ import { ThemeToggle } from "./theme-toggle";
 import { Button, buttonVariants } from "../ui/button";
 import { useConvexAuth } from "convex/react";
 import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+// import { useTransition } from "react";
 
 export function Navbar() {
+  // const [isPending, startTransition] = useTransition();
   const { isAuthenticated, isLoading } = useConvexAuth();
+  const router = useRouter();
   return (
     <nav className="w-full flex items-center justify-between py-5">
       <div className="flex items-center gap-8">
@@ -46,9 +51,23 @@ export function Navbar() {
       <div className="flex items-center gap-2">
         {isLoading ? null : isAuthenticated ? (
           <>
-            <Button onClick={()=>authClient.signOut({
-              
-            })}>Logout</Button>
+            <Button
+              onClick={() =>
+                authClient.signOut({
+                  fetchOptions: {
+                    onSuccess: () => {
+                      toast.success("Logged out successfully");
+                      router.push("/");
+                    },
+                    onError: (error) => {
+                      toast.error(error.error.message);
+                    },
+                  },
+                })
+              }
+            >
+              Logout
+            </Button>
           </>
         ) : (
           <>
