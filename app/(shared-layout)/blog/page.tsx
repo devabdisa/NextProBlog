fad72cd1ac5bf9e8bb3f698089a62e8c04da77fd
Abdisa.dev <meetabdisa@gmail.com>
2026-01-01 10:48:11 +0300

    Stream Blog posts with react suspense and skeleton ui

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -1,12 +1,13 @@
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default async function BlogPage() {
-  const data = await fetchQuery(api.posts.getPosts);
   return (
     <div className="py-12">
       <div className="pb-12 text-center">
@@ -17,38 +18,66 @@ export default async function BlogPage() {
           Insights, thoughts, and trends from the community.
         </p>
       </div>
-      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {data?.map((post) => (
-          <Card key={post._id} className="pt-0">
-            <div className="relative h-48 w-full overflow-hidden ">
-              <Image
-                src="https://images.unsplash.com/photo-1615412704911-55d589229864?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D"
-                alt="Random unsplash nature image"
-                fill
-                className="rounded-t-lg"
-              />
-            </div>
-            <CardContent>
-              <Link href={`/blog/${post._id}`}>
-                <h1 className="text-2xl font-bold hover:text-primary">
-                  {post.title}
-                </h1>
-              </Link>
-              <p className="text-muted-foreground line-clamp-3">{post.body}</p>
-            </CardContent>
-            <CardFooter>
-              <Link
-                href={`/blog/${post._id}`}
-                className={buttonVariants({
-                  className: "w-full",
-                })}
-              >
-                Read More
-              </Link>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+      <Suspense fallback={<SkeletonLoadingUi />}>
+        <LoadBlogList />
+      </Suspense>
+    </div>
+  );
+}
+
+async function LoadBlogList() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const data = await fetchQuery(api.posts.getPosts);
+
+  return (
+    <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {data?.map((post) => (
+        <Card key={post._id} className="pt-0">
+          <div className="relative h-48 w-full overflow-hidden ">
+            <Image
+              src="https://images.unsplash.com/photo-1615412704911-55d589229864?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D"
+              alt="Random unsplash nature image"
+              fill
+              className="rounded-t-lg"
+            />
+          </div>
+          <CardContent>
+            <Link href={`/blog/${post._id}`}>
+              <h1 className="text-2xl font-bold hover:text-primary">
+                {post.title}
+              </h1>
+            </Link>
+            <p className="text-muted-foreground line-clamp-3">{post.body}</p>
+          </CardContent>
+          <CardFooter>
+            <Link
+              href={`/blog/${post._id}`}
+              className={buttonVariants({
+                className: "w-full",
+              })}
+            >
+              Read More
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+function SkeletonLoadingUi() {
+  return (
+    <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {[...Array(6)].map((_, i) => (
+        <div key={i} className="flex flex-col space-y-3">
+          <Skeleton className="h-48 w-full rounded-xl" />
+          <div className="space-y-2 flex flex-col">
+            <Skeleton className="h-6 w-3/4" />
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-2/3" />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
