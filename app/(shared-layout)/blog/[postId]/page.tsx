0bba371df926b6e9da2e40cf73dd841f134763a0
Abdisa.dev <meetabdisa@gmail.com>
2026-01-04 15:32:44 +0300

    Multilayer authorization

--- app/(shared-layout)/blog/[postId]/page.tsx
+++ app/(shared-layout)/blog/[postId]/page.tsx
@@ -10,6 +10,7 @@ import { ArrowLeft } from "lucide-react";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 interface PostIdRouteProps {
   params: Promise<{
@@ -48,6 +49,10 @@ export default async function PostIdRoute({ params }: PostIdRouteProps) {
     }),
     await fetchQuery(api.presence.getUserId, {}, { token }),
   ]);
+
+  if (!userId) {
+    return redirect("/auth/login");
+  }
 
   if (!post) {
     return (
